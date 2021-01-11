bfc2543995c63e7316e9b5f1f0c2a017322e4801
Amber Mattis <amber.mattis2@gmail.com>
2021-01-10 19:55:55 -0600

    java practice

--- src/CodingBat.java
+++ src/CodingBat.java
@@ -13,9 +13,18 @@ public class CodingBat {
         return false;
     }
 
+    public static int sumDouble(int a, int b) {
+        if(a == b){
+            return (a + b) * 2;
+        }
+        return a + b;
+    }
+
     public static void main(String[] args) {
         System.out.println(sleepIn(false,false));
         System.out.println(sleepIn(true,false));
         System.out.println(monkeyTrouble(true,true));
+        System.out.println(sumDouble(10,10));
+        System.out.println(sumDouble(8,4));
     }
 }
