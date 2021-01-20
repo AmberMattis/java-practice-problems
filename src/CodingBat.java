b68d21463bf792abb0aac1c2faa1e0389de96a8b
Amber Mattis <amber.mattis2@gmail.com>
2021-01-19 19:16:45 -0600

    java practice

--- src/CodingBat.java
+++ src/CodingBat.java
@@ -59,6 +59,10 @@ public class CodingBat {
         return false;
     }
 
+    public boolean nearHundred(int n) {
+        return ((Math.abs(100-n) <=10) || (Math.abs(200 - n) <= 10));
+    }
+
 
 
 }
