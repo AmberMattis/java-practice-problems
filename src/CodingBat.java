cef8544e17cc5c36201ba67fd8ce3e7051e07385
Amber Mattis <amber.mattis2@gmail.com>
2021-01-20 08:05:37 -0600

    practice

--- src/CodingBat.java
+++ src/CodingBat.java
@@ -151,6 +151,28 @@ public class CodingBat {
         return ((a >= 13 && a <= 19) || (b >= 13 && b <= 19 ) || (c >= 13 && c <= 19));
     }
 
+    public boolean loneTeen(int a, int b) {
+        if((a >= 13 && a <= 19) && (b >= 13 && b <= 19)){
+            return false;
+        } else if
+        ((a >= 13 && a <= 19) || (b >= 13 && b <= 19)){
+            return true;
+        }
+        return false;
+    }
+
+    //above function  refactored
+    public boolean loneTee2n(int a, int b) {
+        // Store teen-ness in boolean local vars first. Boolean local
+        // vars like this are a little rare, but here they work great.
+        boolean aTeen = (a >= 13 && a <= 19);
+        boolean bTeen = (b >= 13 && b <= 19);
+
+        return (aTeen && !bTeen) || (!aTeen && bTeen);
+        // Translation: one or the other, but not both.
+        // Alternately could use the Java xor operator, but it's obscure.
+    }
+
 
 
 
