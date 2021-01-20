7152a12c35b05bd425ad13d1916406492cd65bd2
Amber Mattis <amber.mattis2@gmail.com>
2021-01-19 19:57:32 -0600

    practice

--- src/CodingBat.java
+++ src/CodingBat.java
@@ -85,6 +85,16 @@ public class CodingBat {
         return front + back;
     }
 
+    public String frontBack(String str) {
+
+        if(str.length() <= 1) return str;
+        String mid = str.substring(1,str.length()-1);
+
+        return str.charAt(str.length()-1) + mid + str.charAt(0);
+
+    }
+
+
 
 
 
