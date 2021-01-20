534a8fb6f641dd8adbe5ea97530be9654bcd4d7b
Amber Mattis <amber.mattis2@gmail.com>
2021-01-19 19:32:32 -0600

    practice

--- src/CodingBat.java
+++ src/CodingBat.java
@@ -63,6 +63,15 @@ public class CodingBat {
         return ((Math.abs(100-n) <=10) || (Math.abs(200 - n) <= 10));
     }
 
+    public boolean posNeg(int a, int b, boolean negative) {
+        if (negative) {
+            return (a < 0 && b < 0);
+        }
+        else {
+            return ((a < 0 && b > 0) || (a > 0 && b < 0));
+        }
+    }
+
 
 
 }
