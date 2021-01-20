51e9184560c9dc6dd39f7c590a49d727ca43ec06
Amber Mattis <amber.mattis2@gmail.com>
2021-01-19 19:38:32 -0600

    practice

--- src/CodingBat.java
+++ src/CodingBat.java
@@ -72,6 +72,14 @@ public class CodingBat {
         }
     }
 
+    public String notString(String str){
+        if (str.length() >= 3 && str.substring(0,3).equals("not")){
+            return str;
+        }
+        return "not " + str;
+    }
+
+
 
 
 }
