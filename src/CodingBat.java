a7422abd3f536611ede5a6efecca94f12bc3ccaa
Amber Mattis <amber.mattis2@gmail.com>
2021-01-20 20:37:20 -0600

    practice

--- src/CodingBat.java
+++ src/CodingBat.java
@@ -193,6 +193,20 @@ public class CodingBat {
         return (str.length() >= 3 && str.substring(1,3).equals("ix"));
     }
 
+    public String startOz(String str) {
+        String result = "";
+
+        if (str.length() >= 1 && str.charAt(0)=='o') {
+            result = result + str.charAt(0);
+        }
+
+        if (str.length() >= 2 && str.charAt(1)=='z') {
+            result = result + str.charAt(1);
+        }
+
+        return result;
+
+
 
 
 
