a9ffee4dc78fdca3a49931a365b5e1c3b4f49259
Amber Mattis <amber.mattis2@gmail.com>
2021-01-20 20:15:01 -0600

    practice

--- src/CodingBat.java
+++ src/CodingBat.java
@@ -180,6 +180,13 @@ public class CodingBat {
         return str;
     }
 
+    public boolean mixStart(String str) {
+        if(str.length() >= 3 && str.substring(1,3).equals("ix")){
+            return true;
+        }
+        return false;
+    }
+
 
 
 
