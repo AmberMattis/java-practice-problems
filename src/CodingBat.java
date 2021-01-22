d6a2669f979ab67f511dc81d3a9ded73d938bf42
Amber Mattis <amber.mattis2@gmail.com>
2021-01-21 21:03:33 -0600

    practice

--- src/CodingBat.java
+++ src/CodingBat.java
@@ -285,6 +285,22 @@ public class CodingBat {
     }
 
 
+    //above code refactored
+    public int max1020(2int a, int b) {
+        // First make it so the bigger value is in a
+        if (b > a) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        // Knowing a is bigger, just check a first
+        if (a >= 10 && a <= 20) return a;
+        if (b >= 10 && b <= 20) return b;
+        return 0;
+    }
+
+
 
 
 
