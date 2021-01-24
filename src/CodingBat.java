1a468e9d552b8104296bf962f1b9005e289f67dd
Amber Mattis <amber.mattis2@gmail.com>
2021-01-23 19:44:06 -0600

    practice

--- src/CodingBat.java
+++ src/CodingBat.java
@@ -339,6 +339,20 @@ public class CodingBat {
         return result;
     }
 
+    public String frontTimes(String str, int n) {
+        int frontLen = 3;
+        if(frontLen > str.length()){
+            frontLen = str.length();
+        }
+        String front = str.substring(0, frontLen);
+
+        String result = "";
+        for(int i =0; i < n; i++){
+            result += front;
+        }
+        return result;
+    }
+
 
 
 
