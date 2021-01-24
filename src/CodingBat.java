ef563c3f765f75ab6afe93cf72421221f5784a21
Amber Mattis <amber.mattis2@gmail.com>
2021-01-23 19:56:03 -0600

    practice

--- src/CodingBat.java
+++ src/CodingBat.java
@@ -353,6 +353,16 @@ public class CodingBat {
         return result;
     }
 
+    int countXX(String str) {
+        int count = 0;
+        for(int i = 0; i < str.length()-1; i++){
+            if(str.substring(i, i+2).equals("xx")){
+                count++;
+            }
+        }
+        return count;
+    }
+
 
 
 
