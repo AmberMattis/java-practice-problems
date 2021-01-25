3c1c68ac66c5b3244aa5af903a755ac33d4e436f
Amber Mattis <amber.mattis2@gmail.com>
2021-01-24 21:35:46 -0600

    practice

--- src/CodingBat.java
+++ src/CodingBat.java
@@ -378,6 +378,19 @@ public class CodingBat {
         // return x.startsWith("xx");
     }
 
+    public String stringBits(String str) {
+        public String stringBits(String str) {
+            String result = "";
+            // Note: the loop increments i by 2
+            for (int i=0; i<str.length(); i+=2) {
+                result = result + str.substring(i, i+1);
+                // Alternately could use str.charAt(i)
+            }
+            return result;
+        }
+
+    }
+
 
 
 
