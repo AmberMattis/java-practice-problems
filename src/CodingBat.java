16997dc4f4afc2dbc56c2b605324c6b18ce74f3f
Amber Mattis <amber.mattis2@gmail.com>
2021-01-26 07:46:12 -0600

    practice

--- src/CodingBat.java
+++ src/CodingBat.java
@@ -402,6 +402,17 @@ public class CodingBat {
         return result;
     }
 
+    public String stringSplosion(String str) {
+        String result = "";
+        //On each iteration we want to ass the substring of the chars 0,i
+        //create a for loop to iterate over every characture in the string
+        for(int i = 0; i < str.length(); i++){
+            result = result + str.substring(0, i+1);
+        }
+        return result;
+    }
+
+
 
 
 
