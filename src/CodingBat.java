f10129665105e8d41427c1de634eb7eddcd0c8df
Amber Mattis <amber.mattis2@gmail.com>
2021-01-28 20:04:26 -0600

    practice

--- src/CodingBat.java
+++ src/CodingBat.java
@@ -454,6 +454,14 @@ public class CodingBat {
         return strings;
     }
 
+    //Given a list of strings, return a list where each string is replaced by 3 copies of the string concatenated together.
+    public List<String> copies3(List<String> strings) {
+        strings.replaceAll(n -> n + n + n);
+        return strings;
+    }
+
+
+
 
 
 
