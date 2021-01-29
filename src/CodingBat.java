8749242189df518f27f05786962e396b33e4bd44
Amber Mattis <amber.mattis2@gmail.com>
2021-01-29 14:46:02 -0600

    practice

--- src/CodingBat.java
+++ src/CodingBat.java
@@ -466,6 +466,13 @@ public class CodingBat {
         return strings;
     }
 
+    //Given a list of integers, return a list where each integer is added to 1 and the result is multiplied by 10
+    public List<Integer> math1(List<Integer> nums) {
+        nums.replaceAll(n -> (n + 1) * 10);
+        return nums;
+    }
+
+
 
 
 
