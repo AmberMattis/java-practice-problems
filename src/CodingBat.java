d30b1b5a1f58485e55209500df65d55ea63ecbb2
Amber Mattis <amber.mattis2@gmail.com>
2021-01-28 19:51:28 -0600

    practice

--- src/CodingBat.java
+++ src/CodingBat.java
@@ -448,6 +448,12 @@ public class CodingBat {
         return nums;
     }
 
+    // Given a list of strings, return a list where each string has "*" added at its end.
+    public List<String> addStar(List<String> strings) {
+        strings.replaceAll(n -> n + "*");
+        return strings;
+    }
+
 
 
 
