a2f9878732f238c17aaa26cd0d03fdcf34616d5b
Amber Mattis <amber.mattis2@gmail.com>
2021-01-29 14:40:22 -0600

    practice

--- src/CodingBat.java
+++ src/CodingBat.java
@@ -460,6 +460,14 @@ public class CodingBat {
         return strings;
     }
 
+    //Given a list of strings, return a list where each string has "y" added at its start and end.
+    public List<String> moreY(List<String> strings) {
+        strings.replaceAll(n -> "y" + n + "y");
+        return strings;
+    }
+
+
+
 
 
 
