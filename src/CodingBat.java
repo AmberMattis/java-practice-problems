064d6c3bf910dc2a9936e698946f5eff3ad692f9
Amber Mattis <amber.mattis2@gmail.com>
2021-01-30 22:31:35 -0600

    practice

--- src/CodingBat.java
+++ src/CodingBat.java
@@ -478,6 +478,17 @@ public class CodingBat {
         return nums;
     }
 
+    //Given an array of ints, return the number of 9's in the a
+    public int arrayCount9(int[] nums) {
+        int count = 0;
+        for (int i=0; i<nums.length; i++) {
+            if (nums[i] == 9) {
+                count++;
+            }
+        }
+        return count;
+    }
+
 
 
 
