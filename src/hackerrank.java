d241eaacadd44f7aeacbd43e52da8d962ad0f913
Amber Mattis <amber.mattis2@gmail.com>
2021-02-03 21:30:06 -0600

    practice

--- src/hackerrank.java
+++ src/hackerrank.java
@@ -90,6 +90,19 @@ public static void main(String[] args) {
 
 
 
+//Given an integer, , print its first  multiples. Each multiple  (where ) should
+// be printed on a new line in the form: N x i = result.
+
+    int N = scan.nextInt();
+    scan.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+    for(int i = 1; i <=10; i++)
+        System.out.printf("%d x %d = %d%n", N, i, N*i);
+
+    scan.close();
+
+
+
 
 }
 
