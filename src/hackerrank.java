53ddb14fea4df4760c70d151c0c36545852e913f
Amber Mattis <amber.mattis2@gmail.com>
2021-02-03 20:42:45 -0600

    practice

--- src/hackerrank.java
+++ src/hackerrank.java
@@ -21,7 +21,6 @@ public static void main(String[] args) {
     int N = scan.nextInt();
     scan.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-    scan.close();
 
     boolean even = N % 2 == 0;
     boolean odd = N % 2 != 0;
@@ -38,6 +37,30 @@ public static void main(String[] args) {
     else if(even && N > 20){
         System.out.println("Not Weird");
     }
+
+    System.out.println("===========================================");
+
+//   At the end of each line of the provided input, there is an invisible "\n" which is called a newline character. This basically represents what happens when you press the "Enter" key when typing.
+//
+//1) The first scan.nextInt() grabs the first "int" from the input. It still leaves the "\n" on that line.
+//
+//2) The second scan.nextDouble() skips over that "\n" and grabs the first "double" it sees.
+//
+//3) Now is the tricky part. It turns out scan.nextLine() works differently than scan.nextInt() and scan.nextDouble() in that it doesn't skip over any "\n" newline characters. What it does is it reads and returns all characters until it reaches the "\n" (which it gets rid of), and stops. In the scenario above, since "\n" is the first thing we see, we get an empty String back on our 1st call to scan.nextLine().
+//
+//4) Our next call to scan.nextLine() will start on line 3 of input and grab the String we want.
+
+
+    int i = scan.nextInt();
+    double d = scan.nextDouble();
+
+    scan.nextLine();
+    String s = scan.nextLine();
+
+    System.out.println("String: " + s);
+    System.out.println("Double: " + d);
+    System.out.println("Int: " + i);
+
 
 }
 
