084948469fbddcf6405b314d19943fcd37dc70a9
Amber Mattis <amber.mattis2@gmail.com>
2021-02-03 21:10:15 -0600

    practice

--- src/hackerrank.java
+++ src/hackerrank.java
@@ -79,6 +79,17 @@ public static void main(String[] args) {
     System.out.println("================================");
 
 
+    System.out.println("================================");
+    for(int i=0;i<3;i++)
+    {
+        String s2=scan.next();
+        int y=scan.nextInt();
+        System.out.printf("%-15s%03d%n", s2, y);
+    }
+    System.out.println("================================");
+
+
+
 
 }
 
