9d3d211729f879c6491603eecabb3280f56a5cbc
Amber Mattis <amber.mattis2@gmail.com>
2021-02-13 15:42:06 -0600

    conditionals practice

--- src/UdemyJavaCourse/IfStatements.java
+++ src/UdemyJavaCourse/IfStatements.java
@@ -11,5 +11,18 @@ public class IfStatements {
         if(score == 5000){                                                  // --- In an if statement, the curly brackets and the code inside is known as a CODE BLOCK
             System.out.println("Your score is " + score + "!");             // --- Code block can acess variables created outside of the code block
         }                                                                   // --- You cannot, outside of the code block access variables created inside of the code block, it will be out of scope
+
+        int score2 = 10000;
+        int levelComplete = 8;
+        int bonus2 = 200;
+
+        if(score2 == 10000){
+            int finalScore = score2 + (levelComplete * bonus2);
+            System.out.println("Your final score is: " + finalScore + "!");
+        }
+
+
     }
+
+
 }
