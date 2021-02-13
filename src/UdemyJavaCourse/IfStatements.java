32493aec9655d5542fbca305e4d4805b5fc79604
Amber Mattis <amber.mattis2@gmail.com>
2021-02-13 15:36:06 -0600

    code block scope notes

--- src/UdemyJavaCourse/IfStatements.java
+++ src/UdemyJavaCourse/IfStatements.java
@@ -9,7 +9,7 @@ public class IfStatements {
         int bonus = 100;
 
         if(score == 5000){                                                  // --- In an if statement, the curly brackets and the code inside is known as a CODE BLOCK
-            System.out.println("Your score is " + score + "!");
-        }
+            System.out.println("Your score is " + score + "!");             // --- Code block can acess variables created outside of the code block
+        }                                                                   // --- You cannot, outside of the code block access variables created inside of the code block, it will be out of scope
     }
 }
