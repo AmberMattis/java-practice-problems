845983d8f94eed437cf828e89d22036b4dc09771
Amber Mattis <amber.mattis2@gmail.com>
2021-02-13 15:12:37 -0600

    code block notes

---
+++ src/UdemyJavaCourse/IfStatements.java
@@ -0,0 +1,15 @@
+package UdemyJavaCourse;
+
+public class IfStatements {
+
+    public static void main(String[] args) {
+        boolean gameOver = true;
+        int score = 5000;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if(score == 5000){                                                  // --- In an if statement, the curly brackets and the code inside is known as a CODE BLOCK
+            System.out.println("Your score is " + score + "!");
+        }
+    }
+}
